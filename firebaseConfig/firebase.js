7d0017401e543e6bb9d3bfd7dfaa4f85d17197b4
Roxyfii <zerogiczgc@gmail.com>
2025-11-22 01:24:58 +0800

    new

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("../google-services (1).json")
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 
 admin.initializeApp({
